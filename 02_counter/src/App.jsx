4d85dac432edd1b62bda4f111a0d80a952be3011
AijazAhmedBZ <aijaz.ahmed.bz@gmail.com>
2023-10-26 07:47:20 +0500

    updated on 26-Oct-23

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  let [counter, setCounter] = useState(1);
+  const [counter, setCounter] = useState(1);
 
   // let counter = 1;
   const addValue = () => {
